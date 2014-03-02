139142b60c92a93c60523a11b05c12938b60109d
yseto <yseto@users.noreply.github.com>
2014-03-02 16:51:32 +0900

    js,concat,minify. via gruntjs

---
+++ js/c.js
@@ -0,0 +1,29 @@
+var nav = $('.nav');
+nav.on('click', 'a[href="#home"]', function() {
+    location.href = "/";
+});
+nav.on('click', 'a[href="#entries"]', function() {
+    location.href = "/entries/";
+});
+nav.on('click', 'a[href="#addasite"]', function() {
+    location.href = "/add/";
+});
+nav.on('click', 'a[href="#subscription"]', function() {
+    location.href = "/subscription/";
+});
+nav.on('click', 'a[href="#settings"]', function() {
+    location.href = "/settings/";
+});
+nav.on('click', 'a[href="#logout"]', function() {
+    location.href = "/?logout=1";
+});
+
+$('#helpmodal').click(function() {
+    $('#helpModal').modal('show');
+});
+
+$('#returntop').click(function() {
+    $('html,body').animate({
+        scrollTop: 0
+    }, 'fast');
+});
