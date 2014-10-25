e56812eaf2703500647a460afa94503d2cb05d27
yseto <ygithub@skycute.sakura.ne.jp>
2014-10-26 01:47:10 +0900

    cleanup bug fix.
    購読先の消去に伴ってゴミが残ってしまっていた
    tidy

--- lib/Gion/Batch/Cleanup.pm
+++ lib/Gion/Batch/Cleanup.pm
@@ -9,30 +9,32 @@ use Gion::DB;
 #  http://stackoverflow.com/questions/8886026/mysql-delete-all-but-latest-x-records
 
 sub run {
-    my $self   = shift;
+    my $self = shift;
 
     my $count;
     my $cmp;
 
     my $db = Gion::DB->new;
     my $engine;
-    if ($self->config->{db}->{dsn} =~ /^(?i:dbi):SQLite:/){
+    if ( $self->config->{db}->{dsn} =~ /^(?i:dbi):SQLite:/ ) {
         $engine = "SQLite";
-    }else{
+    }
+    else {
         $engine = "mysql";
     }
 
-    $count = $db->dbh->select_row('SELECT COUNT(guid) AS t FROM entries');
+    $count       = $db->dbh->select_row('SELECT COUNT(guid) AS t FROM entries');
     $cmp->{olde} = $count->{t};
-    $count = $db->dbh->select_row('SELECT COUNT(guid) AS t FROM stories');
+    $count       = $db->dbh->select_row('SELECT COUNT(guid) AS t FROM stories');
     $cmp->{olds} = $count->{t};
 
     my $rs = $db->dbh->select_all('SELECT id FROM target');
 
-    for (@$rs){
+    for (@$rs) {
         my $id = $_->{id};
-        if ($engine eq 'mysql'){
-            $db->dbh->query("DELETE FROM entries WHERE _id_target = ? AND readflag = 1
+        if ( $engine eq 'mysql' ) {
+            $db->dbh->query(
+                "DELETE FROM entries WHERE _id_target = ? AND readflag = 1
                 AND updatetime < DATE_ADD(CURRENT_TIMESTAMP, INTERVAL -1 DAY)
                 AND 
     	    	pubdate NOT IN (SELECT pubdate FROM 
@@ -40,9 +42,12 @@ sub run {
     	    			WHERE _id_target = ?  AND readflag = 1
     	    			ORDER BY pubdate DESC LIMIT 1
     	    		) x )"
-                , $id, $id );
-        }else{
-            $db->dbh->query("DELETE FROM entries WHERE _id_target = ? AND readflag = 1
+                , $id, $id
+            );
+        }
+        else {
+            $db->dbh->query(
+                "DELETE FROM entries WHERE _id_target = ? AND readflag = 1
                 AND updatetime < date('now','-1 day')
                 AND 
     	    	pubdate NOT IN (SELECT pubdate FROM 
@@ -50,23 +55,47 @@ sub run {
     	    			WHERE _id_target = ? AND readflag = 1
     	    			ORDER BY pubdate DESC LIMIT 1
     	    		) x )"
-                , $id, $id );
+                , $id, $id
+            );
         }
-#       print $id . "\n";
+
+        #       print $id . "\n";
+    }
+
+    my $entries = $db->dbh->select_all("SELECT * FROM entries;");
+    for (@$entries) {
+        my $target =
+          $db->dbh->select_row( "SELECT COUNT(*) AS t FROM target WHERE id = ?",
+            $_->{_id_target} );
+        unless ( $target->{t} > 0 ) {
+            $db->dbh->query( "DELETE FROM entries WHERE _id_target = ?",
+                $_->{_id_target} );
+        }
+    }
+
+    my $feeds = $db->dbh->select_all("SELECT * FROM feeds;");
+    for (@$feeds) {
+        my $target = $db->dbh->select_row(
+            "SELECT COUNT(*) AS t FROM target WHERE _id_feeds = ?",
+            $_->{id} );
+        unless ( $target->{t} > 0 ) {
+            printf "remove target: %s \n", $_->{siteurl};
+            $db->dbh->query( "DELETE FROM feeds WHERE id = ?", $_->{id} );
+        }
     }
 
     $db->dbh->query('OPTIMIZE TABLE entries;') if $engine eq "mysql";
-    $db->dbh->query('DELETE FROM stories WHERE guid NOT IN (SELECT guid FROM entries);');
+    $db->dbh->query(
+        'DELETE FROM stories WHERE guid NOT IN (SELECT guid FROM entries);');
     $db->dbh->query('OPTIMIZE TABLE stories;') if $engine eq "mysql";
 
-    $count = $db->dbh->select_row('SELECT COUNT(guid) AS t FROM entries');
+    $count    = $db->dbh->select_row('SELECT COUNT(guid) AS t FROM entries');
     $cmp->{e} = $count->{t};
-    $count = $db->dbh->select_row('SELECT COUNT(guid) AS t FROM stories');
+    $count    = $db->dbh->select_row('SELECT COUNT(guid) AS t FROM stories');
     $cmp->{s} = $count->{t};
 
     printf "entries %d -> %d \n", $cmp->{olde}, $cmp->{e};
     printf "stories %d -> %d \n", $cmp->{olds}, $cmp->{s};
-
 }
 
 1;
