c3366b594ae0251fe49a900a45a3da9c8e7a844c
yseto <yseto@users.noreply.github.com>
2014-10-26 01:53:05 +0900

    購読先 リレーション変更
    - この修正は過去のテーブルとの互換性がなくなります。
    - 複数のユーザで複数の購読先を登録した場合に発生していた重複を排除したはずです。
    - この修正に伴い、クローラのオプションを変更しました。
    - クローラで更新が頻繁なものだけをクロールするオプションを付加しました。
      - この機能は実験段階です。

--- lib/Gion/Web/Inf.pm
+++ lib/Gion/Web/Inf.pm
@@ -12,16 +12,15 @@ sub get_categories {
     my $self = shift;
     my $db   = $self->app->dbh;
 
-    my $rs = $db->dbh->select_all(
-        "SELECT COUNT(0) AS c, cc.id AS i, cc.name AS n
+    my $rs = $db->dbh->select_all( "
+        SELECT COUNT(0) AS c, cc.id AS i, cc.name AS n
         FROM entries
         INNER JOIN target AS t ON entries._id_target = t.id
         INNER JOIN categories AS cc ON t._id_categories = cc.id 
         WHERE readflag != 1 AND cc.user = ?
         GROUP BY cc.id
-        ORDER BY cc.name ASC",
-        , $self->session('username')
-    );
+        ORDER BY cc.name ASC
+        ", $self->session('username') );
     $self->render( json => $rs );
 }
 
@@ -34,17 +33,19 @@ sub get_entries {
     my $res = $validator->check( cat => [ 'NOT_NULL', 'UINT' ], );
     return $self->render( json => [] ) if $validator->has_error;
 
-    my $id = $data->{cat};
-    my $cfg = $db->dbh->select_row("SELECT * FROM user WHERE id = ?",
-        $self->session('username'));
+    my $id  = $data->{cat};
+    my $cfg = $db->dbh->select_row( "SELECT * FROM user WHERE id = ?",
+        $self->session('username') );
 
     if ( $id == 0 ) {
-        my $rs = $db->dbh->select_row( "SELECT c.id AS id FROM entries 
+        my $rs = $db->dbh->select_row(
+            "SELECT c.id AS id FROM entries 
         INNER JOIN target AS t ON _id_target = t.id
         INNER JOIN categories AS c ON c.id = t._id_categories
         WHERE readflag != 1 AND c.user = ? 
         GROUP BY c.id ORDER BY c.name ASC LIMIT 1",
-        $self->session('username'));
+            $self->session('username')
+        );
 
         return $self->render( json => [] ) unless defined $rs;
         $id = $rs->{id};
@@ -60,26 +61,30 @@ sub get_entries {
         INNER JOIN target AS t ON _id_target = t.id
         INNER JOIN stories AS s ON s.guid = e.guid
         WHERE t._id_categories = ? AND readflag != 1 AND e.user = ?
-        ORDER BY pubDate DESC", $id, $self->session('username'));
+        ORDER BY pubDate DESC", $id, $self->session('username')
+    );
 
     for (@$rs) {
         my $rs2 = $db->dbh->select_row(
-            "SELECT title FROM target WHERE id = ?", $_->{_id_target});
+"SELECT f.title FROM target AS t INNER JOIN feeds AS f ON t._id_feeds = f.id WHERE t.id = ?",
+            $_->{_id_target}
+        );
         my $url = $_->{url};
 
         if ( $cfg->{noreferrer} == 1 ) {
             my $str = encode( 'utf-8', $url );
             $str =~ s/([^0-9A-Za-z!'()*\-._~])/sprintf("%%%02X", ord($1))/eg;
             my $_redirector = $redirector;
-            if ($self->config->{redirector}){
+            if ( $self->config->{redirector} ) {
                 $_redirector = $self->config->{redirector};
             }
             $url = $_redirector . $str;
         }
 
-        my $pd = Time::Piece->strptime($_->{pubDate}, '%Y-%m-%d %H:%M:%S')->strftime('%m/%d %H:%M');
+        my $pd = Time::Piece->strptime( $_->{pubDate}, '%Y-%m-%d %H:%M:%S' )
+          ->strftime('%m/%d %H:%M');
         my $desc = $scrubber->scrub( $_->{description} );
-        $desc = substr($desc,0,$cfg->{numsubstr}) if $cfg->{numsubstr} > 0;
+        $desc = substr( $desc, 0, $cfg->{numsubstr} ) if $cfg->{numsubstr} > 0;
 
         my $h = {
             g => $_->{guid},
@@ -101,26 +106,29 @@ sub get_entries {
 }
 
 sub set_asread {
-    my $self = shift;
-    my $db   = $self->app->dbh;
-    my $data = $self->req->params->to_hash;
+    my $self  = shift;
+    my $db    = $self->app->dbh;
+    my $data  = $self->req->params->to_hash;
     my $array = [];
 
-    if(ref($data->{'g[]'}) eq 'ARRAY'){
-        for(@{$data->{'g[]'}}) {
-            push(@$array,$_);
+    if ( ref( $data->{'g[]'} ) eq 'ARRAY' ) {
+        for ( @{ $data->{'g[]'} } ) {
+            push( @$array, $_ );
         }
-    }else{
-        push(@$array,$data->{'g[]'});
+    }
+    else {
+        push( @$array, $data->{'g[]'} );
     }
 
-    for(@$array){
-        $self->app->log->info(sprintf("ASREAD %s\t%s", $self->session('username'), $_));
+    for (@$array) {
+        $self->app->log->info(
+            sprintf( "ASREAD %s\t%s", $self->session('username'), $_ ) );
         $db->query(
             "UPDATE entries
             SET readflag = 1, updatetime = CURRENT_TIMESTAMP
             WHERE readflag = 0 AND user = ? AND guid = ?",
-            $self->session('username'), $_) if 1 == 1;    # TODO COMMENT OUT
+            $self->session('username'), $_
+        ) if 1 == 1;    # TODO COMMENT OUT
     }
     $self->render( text => "OK" );
 }
@@ -129,26 +137,30 @@ sub get_targetlist {
     my $self = shift;
     my $db   = $self->app->dbh;
 
-    my $cfg = $db->dbh->select_row("SELECT * FROM user WHERE id = ?",
-        $self->session('username'));
+    my $cfg = $db->dbh->select_row( "SELECT * FROM user WHERE id = ?",
+        $self->session('username') );
 
     my $hash = $db->dbh->select_all(
-        "SELECT id AS i, name AS n FROM categories WHERE user = ? ORDER BY name ASC;",
-        $self->session('username'));
+"SELECT id AS i, name AS n FROM categories WHERE user = ? ORDER BY name ASC;",
+        $self->session('username')
+    );
 
     my $rs = $db->dbh->select_all(
-        "SELECT t.id, t.title, t._id_categories, t.http_status, t.siteurl 
-        FROM target AS t INNER JOIN categories AS c ON _id_categories = c.id 
-        WHERE c.user = ? ORDER BY title ASC", $self->session('username'));
+        "SELECT f.id, f.title, t._id_categories, f.http_status, f.siteurl 
+        FROM target AS t 
+        INNER JOIN feeds AS f ON _id_feeds = f.id 
+        WHERE t.user = ? ORDER BY title ASC",
+        $self->session('username')
+    );
 
     my $hash_c;
-    for(@$rs){
+    for (@$rs) {
         my $url = $_->{siteurl};
         if ( $cfg->{noreferrer} == 1 ) {
             my $str = encode( 'utf-8', $_->{siteurl} );
             $str =~ s/([^0-9A-Za-z!'()*\-._~])/sprintf("%%%02X", ord($1))/eg;
             my $_redirector = $redirector;
-            if ($self->config->{redirector}){
+            if ( $self->config->{redirector} ) {
                 $_redirector = $self->config->{redirector};
             }
             $url = $_redirector . $str;
@@ -165,32 +177,31 @@ sub get_targetlist {
     $self->render( json => { n => $hash, t => $hash_c } );
 }
 
-
 sub get_pinlist {
     my $self = shift;
     my $db   = $self->app->dbh;
 
-    my $cfg = $db->dbh->select_row(
-        "SELECT * FROM user WHERE id = ?", $self->session('username'));
+    my $cfg = $db->dbh->select_row( "SELECT * FROM user WHERE id = ?",
+        $self->session('username') );
 
     my $rs = $db->dbh->select_all(
         "SELECT s.title AS t, s.url AS u, e.guid AS g, e.updatetime AS m
             FROM entries AS e 
             INNER JOIN target AS tt ON e._id_target = tt.id
-            INNER JOIN categories AS c ON tt._id_categories = c.id
             INNER JOIN stories AS s ON s.guid = e.guid
-            WHERE e.readflag = 2 AND c.user = ?
-            ORDER BY pubDate DESC", $self->session('username'));
+            WHERE e.readflag = 2 AND tt.user = ?
+            ORDER BY pubDate DESC", $self->session('username')
+    );
 
     return $self->render( json => $rs ) if $cfg->{noreferrer} == 0;
 
     my $hash;
-    for(@$rs) {
+    for (@$rs) {
         my $url = $_->{u};
         my $str = encode( 'utf-8', $url );
         $str =~ s/([^0-9A-Za-z!'()*\-._~])/sprintf("%%%02X", ord($1))/eg;
         my $_redirector = $redirector;
-        if ($self->config->{redirector}){
+        if ( $self->config->{redirector} ) {
             $_redirector = $self->config->{redirector};
         }
         $url = $_redirector . $str;
@@ -210,23 +221,26 @@ sub set_pin {
     my $self = shift;
     my $db   = $self->app->dbh;
 
-    my $data = $self->req->params->to_hash;
+    my $data      = $self->req->params->to_hash;
     my $validator = FormValidator::Lite->new( $self->req );
-    my $res = $validator->check(
-        flag => [ 'NOT_NULL', 'UINT' ],
-        pinid => [ 'NOT_NULL' ],
+    my $res       = $validator->check(
+        flag  => [ 'NOT_NULL', 'UINT' ],
+        pinid => ['NOT_NULL'],
     );
-    return $self->render(json => [] ) if $validator->has_error;
+    return $self->render( json => [] ) if $validator->has_error;
 
     my $flag = 1;
     $flag = 2 if $data->{flag} == 1;
 
-    $self->app->log->info(sprintf("PIN %s\t%s", $self->session('username'), $data->{pinid}));
+    $self->app->log->info(
+        sprintf( "PIN %s\t%s", $self->session('username'), $data->{pinid} ) );
 
-    $db->dbh->query("UPDATE entries
+    $db->dbh->query(
+        "UPDATE entries
         SET readflag = ?, updatetime = CURRENT_TIMESTAMP
-        WHERE user = ? AND guid = ?", 
-        $flag, $self->session('username'), $data->{pinid} );
+        WHERE user = ? AND guid = ?",
+        $flag, $self->session('username'), $data->{pinid}
+    );
 
     $self->render( text => "OK" );
 }
@@ -235,10 +249,12 @@ sub remove_all_pin {
     my $self = shift;
     my $db   = $self->app->dbh;
 
-    $db->dbh->query("UPDATE entries
+    $db->dbh->query(
+        "UPDATE entries
         SET readflag = 1, updatetime = CURRENT_TIMESTAMP
         WHERE readflag = 2 AND user = ?",
-        $self->session('username'));
+        $self->session('username')
+    );
     $self->render( text => "OK" );
 }
 
@@ -246,18 +262,19 @@ sub update_password {
     my $self  = shift;
     my $db    = $self->app->dbh;
     my $data  = $self->req->params->to_hash;
-    my $valid = FormValidator::Lite->new($self->req);
+    my $valid = FormValidator::Lite->new( $self->req );
     my $res   = $valid->check(
-        password_old => ['NOT_NULL',[qw/LENGTH 8 255/]],
-        {passwd => [qw/password passwordc/]} => ['DUPLICATION'],
-        password => ['NOT_NULL',[qw/LENGTH 8 255/]],
+        password_old => [ 'NOT_NULL', [qw/LENGTH 8 255/] ],
+        { passwd => [qw/password passwordc/] } => ['DUPLICATION'],
+        password => [ 'NOT_NULL', [qw/LENGTH 8 255/] ],
     );
-    return $self->render(json => { e => 'error' } ) if $valid->has_error;
+    return $self->render( json => { e => 'error' } ) if $valid->has_error;
 
-    my $strech  = $self->config->{strech} || 500;
-    my $salt    = $self->config->{salt} || "Gion::Util::Auth";
-    my $cfg     = $db->dbh->select_row("SELECT * FROM user WHERE id = ?", $self->session('username'));
-    my $id      = $cfg->{name};
+    my $strech = $self->config->{strech} || 500;
+    my $salt   = $self->config->{salt}   || "Gion::Util::Auth";
+    my $cfg = $db->dbh->select_row( "SELECT * FROM user WHERE id = ?",
+        $self->session('username') );
+    my $id = $cfg->{name};
 
     my $nowauth = Gion::Util::Auth->new(
         strech => $strech,
@@ -266,7 +283,7 @@ sub update_password {
         passwd => Mojo::Util::encode( 'UTF-8', $data->{password_old} ),
     );
     return $self->render( json => { e => 'unmatch now password' } )
-        if $cfg->{pw} ne $nowauth->get_hash;
+      if $cfg->{pw} ne $nowauth->get_hash;
 
     my $auth = Gion::Util::Auth->new(
         strech => $strech,
@@ -275,9 +292,14 @@ sub update_password {
         passwd => Mojo::Util::encode( 'UTF-8', $data->{password} ),
     );
 
-    $self->app->log->info( "Update Passwd: " . $auth->get_hash . "," . $self->session('username') );
-    $db->dbh->query("UPDATE user SET pw = ? WHERE id = ?", $auth->get_hash, $self->session('username'));
+    $self->app->log->info( "Update Passwd: "
+          . $auth->get_hash . ","
+          . $self->session('username') );
+    $db->dbh->query( "UPDATE user SET pw = ? WHERE id = ?",
+        $auth->get_hash, $self->session('username') );
     $self->render( json => { e => 'update password' } );
 }
+
+
 
 1;
